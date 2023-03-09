574988035d6c4dc3c46c2aeb8aa3b07737ff2407
FourEyedRonin <foureyedronin@gmail.com>
2023-03-09 11:57:35 +0100

    Add project files.

---
+++ Code-Yggdrasil/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Code_Yggdrasil
+{
+    
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Window _mainWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _mainWindow = new MainWindow();
+            _mainWindow.Show();
+        }
+
+    }
+}
